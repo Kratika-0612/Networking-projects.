27d674370f1aa41f5c535b2304dc1b8fe464d00e
Kratika <kratikasrivastava06@gmail.com>
2026-07-11 02:35:30 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+HOST='192.168.29.113'
+PORT=9090
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((HOST,PORT))
+server.listen(5)
+while True:
+    print("----------welcome to the Client-server communication----------")
+    communication_socket,address=server.accept()
+    print(f"connected to{address}")
+    message=communication_socket.recv(1024).decode('utf-8')
+    print(f"message from the client{message}")
+    communication_socket.send(f"got your message".encode('utf-8'))
+    communication_socket.close()
+    print(f"connection with{address} ended")
+                                               
+                                               
